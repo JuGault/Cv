3064deed18d4f7c2170101d0fafeb67047825f8e
Julien Gault <jugault45@gmail.com>
2020-03-18 20:53:39 +0100

    personnalisation formation

--- php/experiences.php
+++ php/experiences.php
@@ -19,13 +19,18 @@
     </div>
     <div>
     </div>
-
+    <hr>
     <div id="formation">
         <h3>Formation</h3>
-
-        <a>
-            <img src="Images/ligueombres.jpg" alt="lique des Ombres">
-        </a>
-        <p>Ligue des Ombres</p>
-    </div>
+        <?php
+                $formation = [
+                    'Formation Dévellopeur Web (php/symfony)' => ['lieux' => 'Wild Code School Orléans', 'dates' => ' en (2020)'],
+                    'Formation en interne Affectivité / Citoyenneté / Sexualité' => ['lieux' => 'EPMS Chancepoix 77', 'dates' => ' en (2017)'],
+                    'Formation Moniteur-éducateur' => [ 'lieux' => 'ERTS Olivet', 'dates' => ' de (2013/2015)'],
+                    'Lycée général section Scientifique (Bac S)' => [ 'lieux' => 'Lycée Augustin Thierry à Blois 41', 'dates' => ' en (2009)'],
+                ];
+        ?>
+        <?php foreach ($formation as $whatFormation => $keys) : ?>
+        <p><?php echo $whatFormation . ' à ' ?><?php foreach ($keys as $cara => $valor) : echo $valor ?><?php endforeach; ?></p>
+        <?php endforeach; ?>
 </section>
