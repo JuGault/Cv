cee49cb45d23768e985feb7d08f22fb12f994ba4
Julien Gault <jugault45@gmail.com>
2020-03-19 17:25:27 +0100

    uniformisation section formation/experience

--- php/experiences.php
+++ php/experiences.php
@@ -1,30 +1,26 @@
 <section id="experiences">
-    <div id="enfants">
-        <div>
-            <h3>Références</h3>
-        </div>
         <div id="references">
-            <a id="JL" href="https://www.instagram.com/thedcuniverse/?hl=fr" target="_blank">
-                <img src="Images/logoJL.jpg" alt="logo DC">
-            </a>
+            <h3>Expériences Professionnelles</h3>
+        <?php
 
-            <a href="https://www.instagram.com/superman/" target="_blank">
-                <img src="Images/logoSM.png" alt="Superman">
-            </a>
+        $professionalExperience = [
+            'Moniteur Educateur' => ['EPMS Chancepoix (77)', '(2016/2018 )',],
+            'Educateur'=> ['Action Enfance Amilly (45)', '(2012/2016 )',],
+            'Animateur sociaux culturels' => ['Service enfance Mairie de Chalette sur Loing (45)', '(2011/2012 )',],
+        ];
 
-            <a id="ww" href="https://www.instagram.com/wonderwoman/" target="_blank">
-                <img src="Images/logoWW.png" alt="logo Wonder Woman">
-            </a>
+        ?>
+        <?php foreach ($professionalExperience as $nomPoste => $poste) : ?>
+            <p><?php echo $nomPoste . ' à ' ?><?php foreach ($poste as $info ) : echo $info . "\n" ?><?php endforeach; ?></p>
+        <?php endforeach; ?>
+
         </div>
-    </div>
-    <div>
-    </div>
     <hr>
     <div id="formation">
         <h3>Formation</h3>
         <?php
                 $formation = [
-                    'Formation Dévellopeur Web (php/symfony)' => ['lieux' => 'Wild Code School Orléans', 'dates' => ' en (2020)'],
+                    'Formation Dévellopeur Web (php/symfony)' => ['lieux' => 'Wild Code School Orléans' , 'dates' => ' en (2020)'],
                     'Formation en interne Affectivité / Citoyenneté / Sexualité' => ['lieux' => 'EPMS Chancepoix 77', 'dates' => ' en (2017)'],
                     'Formation Moniteur-éducateur' => [ 'lieux' => 'ERTS Olivet', 'dates' => ' de (2013/2015)'],
                     'Lycée général section Scientifique (Bac S)' => [ 'lieux' => 'Lycée Augustin Thierry à Blois 41', 'dates' => ' en (2009)'],
