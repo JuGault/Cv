c7585351628fc0d3e5ec0ced18c065ea0372c4fa
Julien Gault <jugault45@gmail.com>
2020-03-25 10:48:36 +0100

    add card + css section projects

--- php/projets.php
+++ php/projets.php
@@ -1,6 +1,22 @@
+<?php
+ $allProjects = [
+     'projects'=> ['nameProject'=> 'Cv Batman', 'pictureProject'=>'../Images/Screenshot_2020-03-25 CV de Batman.png', 'linksProject' =>'https://github.com/JuGault/cvBatman',],
+     'projects2'=> ['nameProject'=> 'Cv julien', 'pictureProject'=>'../Images/Screenshot_2020-03-25 CV Julien Gault.png', 'linksProject' =>'https://github.com/JuGault/Cv',],
+ ];
+?>
+
 <section id="projets">
     <div class="titres">
         <h2 id="xp"><span></span>Projets<span></span></h2>
+    </div>
+    <div class="all-projects">
+        <?php foreach ($allProjects as $project) : ?>
+            <article class="projects" >
+                <a href="<?php echo $project['linksProject'] ?>">
+                <img src="<?php echo $project['pictureProject'] ?>" alt="picture <?php echo $project['nameProject'] ?>">
+                <?php echo $project['nameProject'] ?></a>
+            </article>
+        <?php endforeach; ?>
     </div>
 
 </section>
